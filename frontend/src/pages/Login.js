2919186ba1b79047427351db981add554078a7c6
YANY <hurtadoreyesjimena81@gmail.com>
2024-08-20 23:35:24 +0200

    añadir loging a app

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Iniciosesion from '../componentes/Login';
+
+
+function Login() {
+    return (
+        <div className="Login">
+            <Iniciosesion />
+        </div>
+    );
+}
+export default Login;

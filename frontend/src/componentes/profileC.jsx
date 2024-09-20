303a1580836bed49f39d5449a61eda2a40f8ddd7
mila015c <ch2131908@gmail.com>
2024-09-20 23:06:47 +0200

    perfil

--- frontend/src/componentes/profileC.jsx
+++ frontend/src/componentes/profileC.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import mujeres from '../images/mujeres-personaje-dibujos-animados-lindo_81698-1224.avif'
 
 
 
 function ProfileC() {
     return (
         <div className="ProfileC">
-            <section style={{backgroundColor: "#eee"}}>
+            <section style={{backgroundColor: "#d080ff"}}>
   <div className="container py-5">
     <div className="row">
       <div className="col">
         <nav aria-label="breadcrumb" className="bg-body-tertiary rounded-3 p-3 mb-4">
           <ol className="breadcrumb mb-0">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
-            <li className="breadcrumb-item"><a href="#">User</a></li>
-            <li className="breadcrumb-item active" aria-current="page">User Profile</li>
+            <li className="breadcrumb-item"><a href="#">Atrás</a></li>
+         
           </ol>
         </nav>
       </div>
@@ -23,41 +23,20 @@ function ProfileC() {
       <div className="col-lg-4">
         <div className="card mb-4">
           <div className="card-body text-center">
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
-              className="rounded-circle img-fluid" style="width: 150px;"/>
-            <h5 className="my-3">John Smith</h5>
+            <img src={mujeres} alt="avatar"
+              className="rounded-circle img-fluid" style={{width: "150px"}} />
+            <h5 className="my-3">Alejandra Perez</h5>
             <p className="text-muted mb-1">Full Stack Developer</p>
             <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
             <div className="d-flex justify-content-center mb-2">
-              <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary">Follow</button>
-              <button  type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-primary ms-1">Message</button>
+              <button  
+              type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary">Guardar</button>
             </div>
           </div>
         </div>
         <div className="card mb-4 mb-lg-0">
           <div className="card-body p-0">
-            <ul className="list-group list-group-flush rounded-3">
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fas fa-globe fa-lg text-warning"></i>
-                <p className="mb-0">https://mdbootstrap.com</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-github fa-lg text-body"></i>
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-                <p className="mb-0">@mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center p-3">
-                <i className="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-                <p className="mb-0">mdbootstrap</p>
-              </li>
-            </ul>
+            
           </div>
         </div>
       </div>
@@ -66,46 +45,46 @@ function ProfileC() {
           <div className="card-body">
             <div className="row">
               <div className="col-sm-3">
-                <p className="mb-0">Full Name</p>
+                <p className="mb-0">Nombre completo</p>
               </div>
               <div className="col-sm-9">
-                <p className="text-muted mb-0">Johnatan Smith</p>
+                <p className="text-muted mb-0">Alajandra Lopez Castaño</p>
               </div>
             </div>
             <hr/>
             <div className="row">
               <div className="col-sm-3">
-                <p className="mb-0">Email</p>
+                <p className="mb-0">Correo Electronico</p>
               </div>
               <div className="col-sm-9">
-                <p className="text-muted mb-0">example@example.com</p>
+                <p className="text-muted mb-0">aleja097@lpz.com</p>
               </div>
             </div>
             <hr/>
             <div className="row">
               <div className="col-sm-3">
-                <p className="mb-0">Phone</p>
+                <p className="mb-0">Celular</p>
               </div>
               <div className="col-sm-9">
-                <p className="text-muted mb-0">(097) 234-5678</p>
+                <p className="text-muted mb-0">(+57) 3345678684</p>
               </div>
             </div>
             <hr/>
             <div className="row">
               <div className="col-sm-3">
-                <p className="mb-0">Mobile</p>
+                <p className="mb-0">Placa</p>
               </div>
               <div className="col-sm-9">
-                <p className="text-muted mb-0">(098) 765-4321</p>
+                <p className="text-muted mb-0">WWA-892</p>
               </div>
             </div>
             <hr/>
             <div className="row">
               <div className="col-sm-3">
-                <p className="mb-0">Address</p>
+                <p className="mb-0">Vehículo</p>
               </div>
               <div className="col-sm-9">
-                <p className="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                <p className="text-muted mb-0">Mazda 2 negros</p>
               </div>
             </div>
           </div>
@@ -114,31 +93,31 @@ function ProfileC() {
           <div className="col-md-6">
             <div className="card mb-4 mb-md-0">
               <div className="card-body">
-                <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status
+                <p className="mb-4"><span className="text-primary font-italic me-1">Reseñas</span> 
                 </p>
-                <p className="mb-1" style="font-size: .77rem;">Web Design</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                <p className="mb-1" style={{fontSize: ".77rem"}}>Web Design</p>
+                <div className="progress rounded" style={{height:"5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "80%"}} aria-valuenow="80"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Website Markup</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "72%"}} aria-valuenow="72"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>One Page</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "89%"}} aria-valuenow="89"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Mobile Template</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "55%"}} aria-valuenow="55"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                <div className="progress rounded mb-2" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Backend API</p>
+                <div className="progress rounded mb-2" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "66%"}} aria-valuenow="66"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
               </div>
@@ -149,29 +128,29 @@ function ProfileC() {
               <div className="card-body">
                 <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status
                 </p>
-                <p className="mb-1" style="font-size: .77rem;">Web Design</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
+                <p className="mb-1" style={{fontSize: ".77rem"}}>Web Design</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "80%"}} aria-valuenow="80"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Website Markup</p>
+                <div className="progress rounded" style={{height: "5px" }}>
+                  <div className="progress-bar" role="progressbar" style={{width: "72%"}} aria-valuenow="72"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>One Page</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "89%"}} aria-valuenow="89"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                <div className="progress rounded" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Mobile Template</p>
+                <div className="progress rounded" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "55%"}} aria-valuenow="55"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <p className="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                <div className="progress rounded mb-2" style="height: 5px;">
-                  <div className="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66"
+                <p className="mt-4 mb-1" style={{fontSize: ".77rem"}}>Backend API</p>
+                <div className="progress rounded mb-2" style={{height: "5px"}}>
+                  <div className="progress-bar" role="progressbar" style={{width: "66%"}} aria-valuenow="66"
                     aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
               </div>
